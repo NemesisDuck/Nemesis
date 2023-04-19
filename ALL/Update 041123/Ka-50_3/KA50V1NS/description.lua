997c24c458fb755714d18738979a20c455d0e90b
NemesisDuck <Rjhrrsn@gmail.com>
2023-04-19 15:09:35 +0100

    Compact V1

---
+++ ALL/Update 041123/Ka-50_3/KA50V1NS/description.lua
@@ -0,0 +1,47 @@
+livery = {
+
+-- Ka_50_3_Glass - temporary	
+	{"Ka_50_3_Glass", 0,                  "ka_50_glass_d",	true};
+	{"Ka_50_3_Glass", 1,                  "ka_50_glass_n",	true};
+	{"Ka_50_3_Glass", ROUGHNESS_METALLIC, "ka_50_glass_d_RoughMet", true};	 
+	
+-- Ka_50_3_nos
+	{"Ka_50_3_nos", 0,	            "Ka_50_3_nos", false};
+	{"Ka_50_3_nos", 1,	            "ka_50_nos_n",	true};
+	{"Ka_50_3_nos", ROUGHNESS_METALLIC, "ka_50_nos_d_roughmet", true};
+	
+-- Ka_50_3_puzo 
+	{"Ka_50_3_puzo", 0,                  "Ka_50_3_puzo", false};
+	{"Ka_50_3_puzo", 1,                  "ka_50_puzo_n",	true};	
+	{"Ka_50_3_puzo", ROUGHNESS_METALLIC, "ka_50_puzo_d_roughmet", true};
+	
+-- Ka_50_3_hvost
+	{"Ka_50_3_hvost", 0,                  "Ka_50_3_hvost", false};
+	{"Ka_50_3_hvost", 1,                  "ka_50_hvost_n",	true};	
+	{"Ka_50_3_hvost", ROUGHNESS_METALLIC, "ka_50_hvost_d_roughmet", true};	
+	
+-- Ka_50_3_kriliya
+	{"Ka_50_3_kriliya", 0,                  "Ka_50_3_krilo", false};
+	{"Ka_50_3_kriliya", 1,                  "ka_50_krilo_n",	true};	
+	{"Ka_50_3_kriliya", ROUGHNESS_METALLIC, "ka_50_krilo_d_roughmet", true};
+	
+-- Ka_50_3_Shassi
+	{"Ka_50_3_Shassi", 0,                  "Ka_50_3_shassi", false};
+	{"Ka_50_3_Shassi", 1,                  "ka_50_shassi_n",	true};	
+	{"Ka_50_3_Shassi", ROUGHNESS_METALLIC, "ka_50_shassi_d_roughmet", true};		
+	
+-- Ka_50_3_Dvig_Vint_Redukt
+	{"Ka_50_3_Dvig_Vint_Redukt", 0,                  "Ka_50_3_rotor", false};
+	{"Ka_50_3_Dvig_Vint_Redukt", 1,                  "Ka_50_rotor_n",	true};	
+	{"Ka_50_3_Dvig_Vint_Redukt", ROUGHNESS_METALLIC, "Ka_50_rotor_d_roughmet", true}; 
+
+-- Ka_50_3_number	
+	{"Ka_50_3_number",	0,	                "ka_50_numbers", false};
+
+}
+
+-- Made by Beans for Nemesis Squadron
+
+name =       "KA50 V1 Nemesis Squadron"
+countries = {"USA"}
+order     = 1
